439fcccb01b6dc105e6a41aeb1658c27df8a6f5a
arymax <u1221321@gmail.com>
2023-12-12 06:01:00 +0800

    feat: 建立新增餐點資料API

--- src/meal/meal.controller.ts
+++ src/meal/meal.controller.ts
@@ -6,16 +6,18 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFile,
+  Post,
 } from '@nestjs/common';
 import { MealService } from './meal.service';
 import { CreateMealDto } from './dto/create-meal.dto';
 import { UpdateMealDto } from './dto/update-meal.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiResponse } from '@nestjs/swagger';
-@Controller('update-meal')
+import { Meal } from './entities/meal.entity';
+@Controller('meal')
 export class MealController {
   constructor(private readonly mealService: MealService) {}
-  @Patch('/:id')
+  @Patch('update-meal/:id')
   @UseInterceptors(FileInterceptor('image'))
   @ApiBody({ type: UpdateMealDto })
   @ApiResponse({
@@ -34,6 +36,39 @@ export class MealController {
     return {
       message: 'Successfully updated the meal data.',
       data: updatedMeal,
+    };
+  }
+
+  @Post('create-meal/default')
+  @ApiBody({ type: CreateMealDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Successfully create new default meal data.',
+  })
+  async createDefaultMeal(
+    @Body() createMealDto: CreateMealDto,
+  ): Promise<{ message: string; data: any }> {
+    const meal = await this.mealService.createDefaultMeal(createMealDto);
+    return {
+      message: 'Successfully create new default meal data.',
+      data: {
+        image: meal.imageUrl,
+        name: meal.name,
+        describe: meal.describe,
+        price: meal.price,
+        tags:
+          meal.hashtags?.map((tag) => ({ id: tag.id, name: tag.name })) ?? [],
+        selections:
+          meal.selections?.map((selection) => ({
+            id: selection.id,
+            name: selection.name,
+            options:
+              selection.options?.map((option) => ({
+                id: option.id,
+                name: option.name,
+              })) ?? [],
+          })) ?? [],
+      },
     };
   }
 }
--- src/meal/meal.module.ts
+++ src/meal/meal.module.ts
@@ -4,8 +4,10 @@ import { Module } from '@nestjs/common';
 import { MealService } from './meal.service';
 import { MealController } from './meal.controller';
 import { Meal } from './entities/meal.entity';
+import { Category } from '../category/entities/category.entity';
 import { MealSelection } from './entities/meal-selection';
 import { MealSelectionOption } from './entities/meal-selection-option';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([
@@ -13,6 +15,7 @@ import { MealSelectionOption } from './entities/meal-selection-option';
       MealSelection,
       MealSelectionOption,
       Meal,
+      Category,
     ]),
   ],
   controllers: [MealController],
