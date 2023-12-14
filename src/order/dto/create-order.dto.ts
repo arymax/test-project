fac696ebf33212f92c62c0095e7752697f2a39e3
agry88 <rgok307085@gmail.com>
2023-12-14 11:30:19 +0800

    feat: add create-order dto and add modify get-order dto.

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,30 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateOrderDto {
+  @ApiProperty()
+  user_id: number;
+
+  @ApiProperty()
+  restaurant_id: number;
+
+  @ApiProperty()
+  meals: [
+    {
+      meal_id: number;
+      count: number;
+      selections: [
+        {
+          selection_id: number;
+          option_id: number;
+        },
+      ];
+    },
+  ];
+}
+
+export class CrateOrderResponse {
+  message: string;
+  data: {
+    order_id: number;
+  };
+}
